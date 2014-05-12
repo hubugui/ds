b5327893604b686304abe1bd2e08a4a7196a37ac
hubugui <hubugui@gmail.com>
2014-05-12 11:52:32 +0800

    trie, code style format

--- trie/contact.c
+++ trie/contact.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 #include "trie.h"
 
-int 
+int
 main(int argc, char **argv) {
-	int i;
-	char result[1024], *string;
-	unsigned int result_size = 0;
-	struct trie *t = trie_new();
+    int i;
+    char result[1024], *string;
+    unsigned int result_size = 0;
+    struct trie *t = trie_new();
 
-	if (!t) {
-		printf("trie_new() fail\n");
-		return -1;
-	}
+    if (!t) {
+        printf("trie_new() fail\n");
+        return -1;
+    }
 
-	for (i = 1; i < argc - 1; i++) {
-		if (trie_insert(t, argv[i])) {
-			printf("fail\n");
-			goto fail;
-		}
-	}
+    for (i = 1; i < argc - 1; i++) {
+        if (trie_insert(t, argv[i])) {
+            printf("fail\n");
+            goto fail;
+        }
+    }
 
-	printf("total string count=%d\n", trie_string_count(t));
-	printf("total node count=%d\n", trie_node_count(t));
-	printf("\nsearching '%s':\n", argv[argc - 1]);
+    printf("total string count=%d\n", trie_string_count(t));
+    printf("total node count=%d\n", trie_node_count(t));
+    printf("\nsearching '%s':\n", argv[argc - 1]);
 
-	string = trie_search(t, argv[argc - 1], result, &result_size, NULL);
-	for (i = 0; string; i++) {
-		printf("%02d:'%s', len=%d\n", i+1, result, result_size);
-		string = trie_search(NULL, argv[argc - 1], result, &result_size, t);
-	}
+    string = trie_search(t, argv[argc - 1], result, &result_size, NULL);
+    for (i = 0; string; i++) {
+        printf("%02d:'%s', len=%d\n", i + 1, result, result_size);
+        string = trie_search(NULL, argv[argc - 1], result, &result_size, t);
+    }
 
-	printf("\n%d results\n", i);
+    printf("\n%d results\n", i);
 fail:
-	trie_delete(t);
-	return 0;
+    trie_delete(t);
+    return 0;
 }
--- trie/trie.c
+++ trie/trie.c
@@ -5,156 +5,156 @@
 #include <string.h>
 
 struct trie_node {
-	struct trie_node *childs[26];
-	struct trie_node **parent;
-	char leaf;
+    struct trie_node *childs[26];
+    struct trie_node **parent;
+    char leaf;
 };
 
 struct trie_search_box {
-	struct trie_node **match_node, **node;
-	unsigned int string_len;
+    struct trie_node **match_node, * *node;
+    unsigned int string_len;
 };
 
 struct trie {
-	struct trie_node *root;
-	unsigned int string_count, node_count;
-	struct trie_search_box sbox;
+    struct trie_node *root;
+    unsigned int string_count, node_count;
+    struct trie_search_box sbox;
 };
 
 #define NODE_INDEX_OF_PARENT(n) ((int64_t) n - (int64_t) (*(*n)->parent)) / sizeof(struct trie_node *)
 #define _trie_node_new() calloc(1, sizeof(struct trie_node))
 
-static void 
+static void
 _trie_node_delete(struct trie_node *node) {
-	if (node) {
-		int i;
+    if (node) {
+        int i;
 
-		for (i = 0; i < 26; i++) {
-			_trie_node_delete(node->childs[i]);
-		}
-		free(node);
-	}
+        for (i = 0; i < 26; i++) {
+            _trie_node_delete(node->childs[i]);
+        }
+        free(node);
+    }
 }
 
 struct trie *
 trie_new(void) {
-	struct trie *trie = calloc(1, sizeof(struct trie));
+    struct trie *trie = calloc(1, sizeof(struct trie));
 
-	if (trie) {
-		trie->root = _trie_node_new();
-		if (!trie->root) {
-			free(trie);
-			trie = NULL;
-		}
-	}
-	return trie;
+    if (trie) {
+        trie->root = _trie_node_new();
+        if (!trie->root) {
+            free(trie);
+            trie = NULL;
+        }
+    }
+    return trie;
 }
 
-void 
+void
 trie_delete(struct trie *trie) {
-	_trie_node_delete(trie->root);
-	free(trie);
+    _trie_node_delete(trie->root);
+    free(trie);
 }
 
-unsigned int 
+unsigned int
 trie_string_count(struct trie *trie) {
-	return trie->string_count;
+    return trie->string_count;
 }
 
-unsigned int 
+unsigned int
 trie_node_count(struct trie *trie) {
-	return trie->node_count;
+    return trie->node_count;
 }
 
-int 
+int
 trie_insert(struct trie *trie, const char *string) {
-	int chr;
-	struct trie_node **node;
+    int chr;
+    struct trie_node **node;
 
-	for (node = &trie->root; *string != '\0'; string++) {
-		if (!isalpha(*string))
-			goto fail;
+    for (node = &trie->root; *string != '\0'; string++) {
+        if (!isalpha(*string))
+            goto fail;
 
-		chr = tolower(*string) - 'a';
-		if (!(*node)->childs[chr]) {
-			if (!((*node)->childs[chr] = _trie_node_new()))
-				goto fail;
-			(*node)->childs[chr]->parent = node;
-			trie->node_count++;
-		}
-		node = &(*node)->childs[chr];	
-	}
-	if (node == &trie->root)
-		goto fail;
-	(*node)->leaf = 1;
-	trie->string_count++;
-	return 0;
+        chr = tolower(*string) - 'a';
+        if (!(*node)->childs[chr]) {
+            if (!((*node)->childs[chr] = _trie_node_new()))
+                goto fail;
+            (*node)->childs[chr]->parent = node;
+            trie->node_count++;
+        }
+        node = &(*node)->childs[chr];
+    }
+    if (node == &trie->root)
+        goto fail;    
+    (*node)->leaf = 1;
+    trie->string_count++;
+    return 0;
 fail:
-	/* notice, here don't delete parent node. hope to reuse when insert again. */
-	return -1;
+    /* notice, here don't delete parent node. hope to reuse when insert again. */
+    return -1;
 }
 
-char * 
-trie_search(struct trie *trie, 
-			const char *string, 
-			char *result, 
-			unsigned int *result_size, 
-			struct trie *next_ptr) { 
-	struct trie_node **node;
-	char *origin = result;
-	int i, idx;
+char *
+trie_search(struct trie *trie,
+            const char *string,
+            char *result,
+            unsigned int *result_size,
+            struct trie *next_ptr) {
+    struct trie_node **node;
+    char *origin = result;
+    int i, idx;
 
-	if (trie) {
-		/* find match string, save last node to box */
-		memset(&trie->sbox, 0, sizeof(struct trie_search_box));
-		for (node = &trie->root; *string; string++) {
-			if (!isalpha(*string))
-				goto fail;
-			node = &(*node)->childs[(int) (tolower(*string) - 'a')];
-			if (!(*node))
-				goto fail;
-			trie->sbox.match_node = trie->sbox.node = node;
-			trie->sbox.string_len++;
-			*result++ = *string;
-		}
-		if (trie->sbox.string_len == 0)
-			goto fail;
-		*result_size = trie->sbox.string_len;
-		if ((*node)->leaf) {
-			*result = '\0';
-			return result;
-		}
-	} else {
-		trie = next_ptr;
-		result += *result_size;
-	}
+    if (trie) {
+        /* find match string, save last node to box */
+        memset(&trie->sbox, 0, sizeof(struct trie_search_box));
+        for (node = &trie->root; *string; string++) {
+            if (!isalpha(*string))
+                goto fail;
+            node = &(*node)->childs[(int) (tolower(*string) - 'a')];
+            if (!(*node))
+                goto fail;
+            trie->sbox.match_node = trie->sbox.node = node;
+            trie->sbox.string_len++;
+            *result++ = *string;
+        }
+        if (trie->sbox.string_len == 0)
+            goto fail;
+        *result_size = trie->sbox.string_len;
+        if ((*node)->leaf) {
+            *result = '\0';
+            return result;
+        }
+    } else {
+        trie = next_ptr;
+        result += *result_size;
+    }
 
-	node = trie->sbox.node;
-	idx = 0;
+    node = trie->sbox.node;
+    idx = 0;
 loop:
-	/* child */
-	for (i = idx; i < 26; i++) {
-		if ((*node)->childs[i]) {
-			node = trie->sbox.node = &(*node)->childs[i];
-			*result++ = i + 'a';
-			*result_size += 1;
-			if ((*node)->leaf) {
-				*result = '\0';
-				return origin;
-			}
-			idx = 0;
-			goto loop;
-		}
-	}
-	if (node == trie->sbox.match_node)
-		goto fail;
+    /* child */
+    for (i = idx; i < 26; i++) {
+        if ((*node)->childs[i]) {
+            node = trie->sbox.node = &(*node)->childs[i];
+            *result++ = i + 'a';
+            *result_size += 1;
+            if ((*node)->leaf) {
+                *result = '\0';
+                return origin;
+            }
+            idx = 0;
+            goto loop;
+        }
+    }
+    if (node == trie->sbox.match_node)
+        goto fail;
 
-	/* right brother */
-	result--;
-	*result_size -= 1;
-	idx = NODE_INDEX_OF_PARENT(node) + 1;
-	node = (*node)->parent;
-	goto loop;
+    /* right brother */
+    result--;
+    *result_size -= 1;
+    idx = NODE_INDEX_OF_PARENT(node) + 1;
+    node = (*node)->parent;
+    goto loop;
 fail:
-	return NULL;
+    return NULL;
 }
