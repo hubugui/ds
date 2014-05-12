16d81f895881b5283ae355bcf36a12e1b0676e8d
hubugui <hubugui@gmail.com>
2014-05-12 17:39:47 +0800

    bst

--- binary_search_tree/bst.c
+++ binary_search_tree/bst.c
@@ -1,69 +1,143 @@
-#include <stdio.h>
 #include <memory.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-#include "dst.h"
+#include "bst.h"
 
-struct bst
-{
-	struct bst *left;
-	struct bst *right;
+struct bst_node {
+	struct bst_node *left, *right;
 	int value;
 };
 
-struct bst *bst_create()
-{
-	return calloc(1, sizeof(struct bst));
+struct bst {
+	struct bst_node *root;
+};
+
+#define _bst_node_new()	calloc(1, sizeof(struct bst_node))
+
+static void 
+_bst_node_delete(struct bst_node *node) {
+	if (node->left)
+		_bst_node_delete(node->left);
+	if (node->right)
+		_bst_node_delete(node->right);
+	free(node);
 }
 
-void bst_destroy(struct bst *bst)
-{
-	if (bst)
-	{
-		if (bst->left)
-			bst_destroy(bst->left);
-		if (bst->right)
-			bst_destroy(bst->right);
-		free(bst);
+struct bst * 
+bst_new(void) {
+	return calloc(1, sizeof(struct bst))
+}
+
+void 
+bst_delete(struct bst *bst) {
+	if (bst->root)
+		_bst_node_delete(bst->root);
+	free(bst);
+}
+
+static struct bst_node *  
+_bst_node_find(struct bst *bst, int value, struct bst_node **parent, char *left_child) {
+	struct bst_node *node = bst->root;
+
+    *parent = NULL;
+	while (node) {
+        if (value == node->value)
+            return node;
+        *parent = node;
+		if (value < node->value) {
+			node = node->left;
+            *left_child = 1;
+        } else if (value > node->value) {
+			node = node->right;
+            *left_child = 0;
+        }
 	}
+	return NULL;
 }
 
-void bst_insert(struct bst *bst, int value)
-{
-	
+int 
+bst_find(struct bst *bst, int value) {
+    struct bst_node *parent = NULL;
+    char left_child = 0;
+
+    return _bst_node_find(bst, value, &parent, &left_child) ? 0 : -1;
 }
 
-void bst_delete(struct bst *bst, int value);
+int 
+bst_insert(struct bst *bst, int value) {
+	struct bst_node *node = bst->root;
+	struct bst_node **parent = &bst->root;
 
-struct bst *bst bst_find(struct bst *bst, int value)
-{
-	if (bst)
-	{
-		if (bst->value > value)
-			return bst_find(bst->left);
-		if (bst->value < value)
-			return bst_find(bst->right);
-		return bst;
+	while (node) {
+		if (value < node->value)
+            parent = &node->left;
+		else if (value > node->value)
+            parent = &node->right;
+        node = *parent;
+		return 0;
 	}
-	else
-		return NULL;
+
+    if (*parent = _bst_node_new())
+        (*parent)->value = value;
+    else
+        return -1;
+	return 0;
 }
 
-int bst_value(struct bst *bst)
-{
-	return bst->value;
+int 
+bst_remove(struct bst *bst, int value) {
+    struct bst_node *parent = NULL;
+    char left_child = 0;
+	struct bst_node *node = _bst_node_find(bst, value, &parent, &left_child) ? 0 : -1;
+   
+    if (!node)
+        return -1;
+
+    if (!node->left && !node->right) {
+        if (parent) {
+            if (left_child)
+                parent->left = NULL;
+            else
+                parent->right = NULL;
+        } else
+            bst->root = NULL;
+    } else if (!node->left || !node->right) {
+       if (parent) {
+            if (left_child)
+                parent->left = node->left ? node->left : node->right;
+            else
+                parent->right = node->left ? node->left : node->right;
+       } else
+            bst->root = node->left ? node->left : node->right;
+    } else {
+        
+    }
+
+    free(node);
+    return 0;
 }
 
-int bst_min(struct bst *bst)
-{
-	if (bst->left)
-		return bst_min(bst->left);
-	return bst->value;
+int 
+bst_max(struct bst *bst) {
+	struct bst_node *node = bst->root;
+
+    if (node) {
+    	while (node->right)
+            node = node->right;
+        return node->value;
+    }
+	return 0;
 }
 
-int bst_max(struct bst *bst)
-{
-	if (bst->right)
-		return bst_max(bst->right);
-	return bst->value;
+int 
+bst_min(struct bst *bst) {
+	struct bst_node *node = bst->root;
+
+    if (node) {
+    	while (node->left)
+            node = node->left;
+        return node->value;
+    }
+	return 0;
 }
--- binary_search_tree/bst.h
+++ binary_search_tree/bst.h
@@ -1,17 +1,23 @@
 #ifndef __BST__
 #define __BST__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct bst;
 
-struct bst *bst_create();
-void bst_destroy(struct bst *bst);
+struct bst *bst_new(void);
+void bst_delete(struct bst *bst);
 
-void bst_insert(struct bst *bst, int value);
-void bst_delete(struct bst *bst, int value);
+int bst_insert(struct bst *bst, int value);
+int bst_remove(struct bst *bst, int value);
 
-struct bst *bst bst_find(struct bst *bst, int value);
-int bst_value(struct bst *bst);
-int bst_min(struct bst *bst);
+int bst_find(struct bst *bst, int value);
 int bst_max(struct bst *bst);
+int bst_min(struct bst *bst);
 
+#ifdef __cplusplus
+}
+#endif
 #endif
