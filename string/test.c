11747e4f82a0ffa2ec39af65c1ebd68dcec4bb64
hubugui <hubugui@gmail.com>
2014-05-18 00:55:40 +0800

    src+size=dst

--- string/test.c
+++ string/test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "memmove.h"
 
@@ -22,9 +23,9 @@ main(int argc, char **argv) {
     printf("dst=%s, src=%s, size=%d\n", dst, src, 5);
 
     strcpy(array, "12345678");
-    dst = string;
-    src = "abcd";
-    _memmove(dst, src, 4);
-    printf("dst=%s, src=%s, size=%d\n", dst, src, 4);
+    dst = string + 2;
+    src = string;
+    _memmove(dst, src, 5);
+    printf("dst=%s, src=%s, size=%d\n", dst, src, 5);
     return 0;
 }
