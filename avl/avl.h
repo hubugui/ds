f23adba7625d7414b997c947b530f34775a5f3c9
hubugui <hubugui@gmail.com>
2014-05-30 17:51:33 +0800

    avl

--- avl/avl.h
+++ avl/avl.h
@@ -7,16 +7,21 @@ extern "C" {
 
 struct avl;
 
+typedef int (*cmpare)(void *v1, void *v2);
+
 struct avl *avl_new(void);
 void avl_delete(struct avl *avl);
 
-int avl_insert(struct avl *avl, int value);
-int avl_remove(struct avl *avl, int value);
+void avl_set_cmp(struct avl *avl, compare cmp);
+compare avl_get_cmp(struct avl *avl);
 
-int avl_search(struct avl *avl, int value);
+int avl_insert(struct avl *avl, void *value);
+int avl_remove(struct avl *avl, void *value);
+
+int avl_search(struct avl *avl, void *value);
 int avl_max(struct avl *avl);
 int avl_min(struct avl *avl);
-void avl_dump(struct avl *avl);
+void avl_in_order(struct avl *avl, void (*dump)(void *value));
 
 #ifdef __cplusplus
 }
