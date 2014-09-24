feda0099fdb5265b5d3f859221e8dbb579fa5a1a
hubugui <hubugui@gmail.com>
2014-09-24 17:34:19 +0800

    permutation, avoid funtion return array

--- permutation/permutation.py
+++ permutation/permutation.py
@@ -2,31 +2,33 @@
 
 import sys
 
+def permutation(array, bi, cases):
+	if bi >= len(array):
+		return
 
-def permutation(array, bi, ei):
-	results = []
+	# sub array
+	permutation(array, bi+1, cases)
 
-	if bi > ei:
-		return []
-
-	# first
-	results.append([array[bi]])
-
-	sub_results = permutation(array, bi+1, ei)
-	results += sub_results
-	size = len(sub_results)
+	# plus sub cases
+	size = len(cases)
 	for i in range(0, size):
-		results.append([array[bi]] + sub_results[i])
-	return results
+		cases.append([array[bi]] + cases[i])
+
+	# first item
+	cases.append([array[bi]])
+
+def go(array, cases):
+	permutation(array, 0, cases)
 
 if __name__ == "__main__":
 	size = 4
 	if len(sys.argv) > 1:
 		size = int(sys.argv[1])
+	array = [x for x in range(1, size+1)]
 
 	idx = 1
-	array = [x for x in range(1, size+1)]	
-	results = permutation(array, 0, size - 1)
-	for item in results:
-		print("{}>{}".format(idx, item))
+	cases = []
+	go(array, cases)
+	for case in cases:
+		print("{}>{}".format(idx, case))
 		idx = idx + 1
